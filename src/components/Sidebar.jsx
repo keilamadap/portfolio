bbc3d75cba69bb2664c5644ad167a24be9825545
Keila Amada <keila.amadap@hotmail.com>
2022-10-15 11:31:40 -0300

    Update Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -8,7 +8,7 @@ const Sidebar = () => {
   return (
     <aside id="sidebar">
       <img src={Avatar} alt="Keila-Perfil" />
-      <p className="title">Frontend</p>
+      <p className="title">Desenvolvedora</p>
       <SocialNetwork />
       <InformationContainer />
       <a
